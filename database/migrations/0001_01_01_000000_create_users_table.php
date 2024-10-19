aa7ab0c2ff62a309461c225bb3387365a037ecdc
Triliam <taisbuenovidotto@gmail.com>
2024-10-19 15:49:01 -0300

    11 numbers whatsapp

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('whatsapp_number');
+            $table->string('whatsapp_number', 11);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
